5985871520af59a92d3945499251ec8e1e43f269
Munirul Haikal <munirulhaikal@gmail.com>
2022-02-21 17:52:24 +0800

    Section 9 - 10: Request, Response & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,36 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home.index')->name('home.index');
-Route::view('/contact', 'home.contact')->name('home.contact');
+Route::get('/', [HomeController::class, 'home'])->name('home.index');
+Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel',
-        'is_new' => true,
-        'has_comments' => true
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP',
-        'is_new' => false
-    ],
-    3 => [
-        'title' => 'Intro to Golang',
-        'content' => 'This is a short intro to Golang',
-        'is_new' => false
-    ]
-];
+Route::get('/single', AboutController::class);
 
-Route::get('/posts', function () use ($posts) {
-    return view('posts.index', ['posts' => $posts]);
-});
+Route::resource('posts', PostController::class)->only(['index', 'show']);
 
-Route::get('/posts/{id}', function ($id) use ($posts) {
-    abort_if(!isset($posts[$id]), 404);
-    return view('posts.show', ['post' => $posts[$id]]);
-})->name('posts.show');
+// Route::get('/posts', function () use ($posts) {
+//     dd(request()->all());
+//     return view('posts.index', ['posts' => $posts]);
+// });
+
+// Route::get('/posts/{id}', function ($id) use ($posts) {
+//     abort_if(!isset($posts[$id]), 404);
+//     return view('posts.show', ['post' => $posts[$id]]);
+// })->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}', function ($days_ago = 0) {
     if ($days_ago == 0) {
@@ -50,4 +40,37 @@ Route::get('/recent-posts/{days_ago?}', function ($days_ago = 0) {
     } else {
         return 'Post from ' . $days_ago . ' days ago';
     }
-})->name('posts.recent.index');
+})->name('posts.recent.index')->middleware('auth');
+
+Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
+
+    Route::get('/responses', function () use ($posts) {
+        return response($posts, 201)
+            ->header('Content-Type', 'application/json')
+            ->cookie('MY_COOKIE', 'Piotr Jura', 3600);
+    })->name('responses');
+
+    Route::get('/redirect', function () {
+        return redirect('/contact');
+    })->name('redirect');
+
+    Route::get('/back', function () {
+        return back();
+    })->name('back');
+
+    Route::get('/named-route', function () {
+        return redirect()->route('posts.show', ['id' => 1]);
+    })->name('named-route');
+
+    Route::get('/away', function () {
+        return redirect()->away('https://www.google.com');
+    })->name('away');
+
+    Route::get('/json', function () use ($posts) {
+        return response()->json($posts);
+    })->name('json');
+
+    Route::get('/download', function () {
+        return response()->download(public_path('/daniel.jpg'), 'face.jpg');
+    })->name('download');
+});
