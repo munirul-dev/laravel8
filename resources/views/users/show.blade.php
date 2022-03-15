9f682cc83fed5fe020f80d89feb8bd2f466b7339
Munirul Haikal <munirulhaikal@gmail.com>
2022-03-15 17:04:33 +0800

    Show placeholder for profile picture

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-4">
-            <img src="{{ $user->image ? $user->image->url() : '' }}" alt="Avatar" class="img-thumbnail avatar">
+            <img src="{{ $user->image ? $user->image->url() : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }}" alt="Avatar" class="img-thumbnail avatar">
         </div>
         <div class="col-8">
             <h3>{{ $user->name }}</h3>
